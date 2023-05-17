a1a2970e19e57889810542acecd4a4f73c3f904a
Martin Georgiev <georgiev632@gmail.com>
2023-05-17 15:12:15 +0300

    UI Added

--- app/src/main/java/com/example/quizapp/StartActivity.java
+++ app/src/main/java/com/example/quizapp/StartActivity.java
@@ -1,8 +1,11 @@
 package com.example.quizapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class StartActivity extends AppCompatActivity {
 
@@ -10,5 +13,22 @@ public class StartActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+
+        final AppCompatButton startQuizButton = findViewById(R.id.startQuizButton);
+        final AppCompatButton quitButton = findViewById(R.id.QuitButton);
+
+        startQuizButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(StartActivity.this, MainActivity.class));
+            }
+        });
+
+        quitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 }
